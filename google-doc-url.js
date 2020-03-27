cf23db14612e60cc7dfa06691168926e554ef37e
Sasha Brown <sashadbrown@gmail.com>
2020-03-27 12:36:44 +0000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1nXeED_LxXN7p9PyGOs-ZojjrDhd0UiygiEx8uBQUNBU/edit#gid=0';
